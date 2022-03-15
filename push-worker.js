7902dbcacbe78260c417dd701a0cb53461ef3fc8
David Adamec <david.adamec@airship.com>
2022-03-14 23:01:26 -0700

    Add web push test

---
+++ push-worker.js
@@ -0,0 +1,10 @@
+// 86acbd31cd7c09cf30acb66d2fbedc91daa48b86:1647321446.5155828
+importScripts('https://aswpsdkus.com/notify/v1/ua-sdk.min.js')
+uaSetup.worker(self, {
+  defaultIcon: 'https://i.imgur.com/riDmiEhb.jpg',
+  defaultTitle: 'Hello world',
+  defaultActionURL: 'https://airship.com',
+  appKey: 'GM5pqjRJRDOZx5DyJHm2vQ',
+  token: 'MTpHTTVwcWpSSlJET1p4NUR5SkhtMnZROmZJMXB6RmVwWWtDWXYwNmEzYkl0VGRwMkU0dWcycG85b3QxZEdOU1Aybm8',
+  vapidPublicKey: 'BL6LjDWlnxKcw6fInHfUICajzIWDfeoMitQmTx1EDzQJm00QpnvMjhBeuyZN67aBhb5-g6G-yW0hmNacJ73eLmU='
+})
